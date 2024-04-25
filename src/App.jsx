15ad6fdd20ff67942f84ff63aa77b9ea662a1b94
Deni4kaHun7 <denis.serkes@gmail.com>
2024-04-25 20:03:21 +0200

    string solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,51 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let lastInput = "0"
+  const [output, setOutput] = useState("0")
+
+  function clearOutput() {
+    setOutput("0");
+    lastInput = "0";
+  }
+
+  function changeOutput(event){
+    if(output.includes("=")){
+      clearOutput()
+    }
+
+    const userInput = event.target.innerText;
+    lastInput = userInput;
+    setOutput(prevOutput => prevOutput + userInput)
+  }
+
+  function calculateResult(){
+    const result = eval(output)
+    lastInput = result;
+    setOutput(prevOutput => prevOutput + "=" + result)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="calculator-container">
+      <div id="display">{output}</div>
+      <button onClick={clearOutput} id="clear">AC</button>
+      <button className="operation-btn" id="divide" onClick={changeOutput}>/</button>
+      <button className="operation-btn" id="multiply" onClick={changeOutput}>*</button>
+      <button className="number-btn" id="seven" onClick={changeOutput}>7</button>
+      <button className="number-btn" id="eight" onClick={changeOutput}>8</button>
+      <button className="number-btn" id="nine" onClick={changeOutput}>9</button>
+      <button className="operation-btn" id="subtract" onClick={changeOutput}>-</button>
+      <button className="number-btn" id="four" onClick={changeOutput}>4</button>
+      <button className="number-btn" id="five" onClick={changeOutput}>5</button>
+      <button className="number-btn" id="six" onClick={changeOutput}>6</button>
+      <button className="operation-btn" id="add" onClick={changeOutput}>+</button>
+      <button className="number-btn" id="one" onClick={changeOutput}>1</button>
+      <button className="number-btn" id="two"onClick={changeOutput}>2</button>
+      <button className="number-btn" id="three"onClick={changeOutput}>3</button>
+      <button className="operation-btn" id="equals" onClick={calculateResult}>=</button>
+      <button className="number-btn" id="zero" onClick={changeOutput}>0</button>
+      <button className="number-btn" id="decimal">.</button>
+    </div>
   )
 }
 
